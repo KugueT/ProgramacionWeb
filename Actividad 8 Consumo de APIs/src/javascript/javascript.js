6393a206b6206e9a96d0826d13dd0f1625dc350c
Emmanuel Kugue <91816396+KugueT@users.noreply.github.com>
2021-11-14 03:30:42 -0700

    Correcciones

--- Actividad 8 Consumo de APIs/src/javascript/javascript.js
+++ Actividad 8 Consumo de APIs/src/javascript/javascript.js
@@ -1,5 +1,4 @@
 
-
    function consultarPokemon(id,num){
    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
    .then(function (response){
@@ -21,12 +20,26 @@ function crearpokemon(pokemon,num){
 
    let item = lista.querySelector(`#pokemon-${num}`)
    let imagen= item.getElementsByTagName("img")[0]
+   let imagen2= item.getElementsByTagName("img")[1]
    imagen.setAttribute("src",pokemon.sprites.front_default)
+   imagen2.setAttribute("src",pokemon.sprites.back_default)
+
    let nombre= item.getElementsByTagName ("p")[0]
-    let tipo= item.getElementsByTagName ("p")[1]
+    let id= item.getElementsByTagName ("p")[1]
+    let tipo = item.getElementsByTagName ("p")[2]
+      let tipo2 = item.getElementsByTagName ("p")[3]
+
    nombre.textContent = pokemon.name
-   tipo.textContent = pokemon.stats
-   console.log(tipo)
+   id.textContent = pokemon.id
+   tipo.textContent  = pokemon.types[0].type.name
+   tipo2.textContent  = pokemon.types[1].type.name
+
+
+   
+     
+
+  
 }
+
 
 
